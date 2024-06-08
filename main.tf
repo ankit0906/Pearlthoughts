8b8e12b72fc7edc0d1d4abaf80e1182d466e1fe9
ankit@github <124163400+ankit0906@users.noreply.github.com>
2024-06-08 15:47:21 +0530

    main.tf file for terraform

---
+++ main.tf
@@ -0,0 +1,72 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.${count.index}.0/24"
+  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "hello_world" {
+  family = "hello-world-task"
+  container_definitions = jsonencode([
+    {
+      name  = "hello-world"
+      image = "your-dockerhub-username/hello-world:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
+    }
+  ])
+  requires_compatibilities = ["FARGATE"]
+  memory = "512"
+  cpu    = "256"
+}
+
+resource "aws_ecs_service" "hello_world" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.hello_world.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = aws_subnet.public[*].id
+    security_groups = [aws_security_group.allow_all.id]
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
